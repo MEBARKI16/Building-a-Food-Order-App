3dbf20d6fa55a729f95232c4a83afcdb410e44af
Amine MEBARKI <medaminemebarki@gmail.com>
2022-08-23 23:16:50 +0200

    Adding a Meals Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Layout/Header'
 import {Fragment} from 'react'
+import Meals from './components/Meals/Meals';
 function App() {
   return (
     <Fragment>
       <Header/>
+      <Meals/>
     </Fragment>
   );
 }
